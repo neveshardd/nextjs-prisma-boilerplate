942cbde80f8fd9cb786cd8f0f9cf224084a1e204
neves <118855875+neveshardd@users.noreply.github.com>
2026-06-29 00:29:24 -0300

    fix: remove engine option obsoleta do prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  engine: "classic",
   migrations: {
     path: "prisma/migrations",
   },
